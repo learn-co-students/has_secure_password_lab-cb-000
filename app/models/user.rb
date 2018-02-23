bf84ff2265909cb7b0985f6f877068b979014b17
Heather Christman <efl7a@yahoo.de>
2018-02-23 09:40:02 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ActiveRecord::Base
-  validates :password, presence: true
-  validate :confirm_password
+  has_secure_password
+  # validates :password, presence: true
+  # validate :confirm_password
 
-  def confirm_password
-    if !self.password_confirmation.nil? && self.password != self.password_confirmation
-      errors.add(:password, "Passwords do not match")
-    end
-  end
-
-  def authenticate(password)
-    if self.password == password
-      self
-    end
-  end
+  # def confirm_password
+  #   if !self.password_confirmation.nil? && self.password != self.password_confirmation
+  #     errors.add(:password, "Passwords do not match")
+  #   end
+  # end
+  #
+  # def authenticate(password)
+  #   if self.password == password
+  #     self
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222164648) do
+ActiveRecord::Schema.define(version: 20180223143532) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password"
-    t.string   "password_confirmation"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
