4cea0cf2ad5796ef466a777b61d04108d33956f6
Jeremy Parker <jrobinparker@gmail.com>
2018-10-31 20:24:53 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,25 @@
 class SessionsController < ApplicationController
 
-  def create
-    if @user = User.find_by(username: params[:username])
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      redirect_to '/login'
-    end
+  def new
   end
 
+ def create
+   user = User.find_by(username: params[:user][:username])
+
+   user = user.try(:authenticate, params[:user][:password])
+
+   return redirect_to(controller: 'sessions', action: 'new') unless user
+
+   session[:user_id] = user.id
+
+   @user = user
+
+   redirect_to controller: 'users', action: 'index'
+ end
+
+ def destroy
+   session.delete :user_id
+
+   redirect_to '/'
+ end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,24 +4,19 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new
-    @user.username = params[:user][:username]
-    @user.password = params[:user][:password]
-    if @user.save
-      redirect_to 'root_path'
-    else
-      redirect_to 'users/new'
-    end
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'users', action: 'index'
   end
 
-  private
+ private
 
   def user_params
-    params.require[:user].permit[:username, :password, :password_confirmation]
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
-
+  
 end
