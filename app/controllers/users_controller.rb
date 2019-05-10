fe46e4156471b765e7dad9f9ae3b1a1c12261f44
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 17:07:19 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    redirect_to new_user_path if params[:password] != params[:password_confirmation]
     new_user = User.create(user_params)
     session[:user_id] = new_user.id
     redirect_to home_path
