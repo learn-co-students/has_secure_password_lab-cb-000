e81fc96d1861689beb80412ad125ee144c566ef2
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 17:41:52 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    redirect_to new_user_path if params[:password] != params[:password_confirmation]
-    new_user = User.create(user_params)
-    session[:user_id] = new_user.id
-    redirect_to home_path
+    if params[:user][:password] != params[:user][:password_confirmation] then
+      redirect_to new_user_path
+    else
+      new_user = User.create(user_params)
+      session[:user_id] = new_user.id
+      redirect_to home_path
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20190510160500) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
